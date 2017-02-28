16eb413e05b49f36a562a1e43bc5d682abddf8b0
Tyrone Cragg <tyrone.cragg@gmail.com>
2017-02-28 19:34:17 +0200

    XGB 2

--- model/xgb.py
+++ model/xgb.py
@@ -1,15 +1,18 @@
 #Predict the product review score
+#XGB2
 
 #Load required packages
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import TruncatedSVD
 import matplotlib.pyplot as plt
 import xgboost as xgb
 from sklearn.metrics import mean_squared_error
 from sklearn.decomposition import NMF
 from sklearn.pipeline import make_pipeline
+import gc
 
 #Read in csv
 reviews = pd.read_csv('Reviews.csv', index_col = 'Id', usecols = ['Id', 'Summary', 'Text', 'Score'])
@@ -17,9 +20,10 @@ reviews = pd.read_csv('Reviews.csv', index_col = 'Id', usecols = ['Id', 'Summary
 #Drop duplicate score-text values
 reviews = reviews.drop_duplicates(subset = ['Summary', 'Text', 'Score'])
 
+#Remove Text
 reviews = reviews.drop('Text', axis = 'columns')
 
-#Create text - convert to lower case and remove line breaks
+#Create text - drop duplicates, convert to lower case and remove line breaks
 reviews['Summary'] = reviews['Summary'].str.lower().str.replace('<br />','')
 
 #Replace nan with ""
@@ -37,15 +41,42 @@ train, test, train_score, test_score = train_test_split(reviews, score, train_si
 #dtm
 vectorizer = CountVectorizer(min_df = 0.0005, max_df = 1.0, ngram_range = (1, 3), stop_words = 'english')
 vectorizer.fit(train['Summary'])
-train_summary_dtm = vectorizer.transform(train['Summary'])
-test_summary_dtm = vectorizer.transform(test['Summary'])
+train_text_dtm = vectorizer.transform(train['Summary'])
+test_text_dtm = vectorizer.transform(test['Summary'])
 
-train = train_summary_dtm
-test = test_summary_dtm
+train = train_text_dtm
+test = test_text_dtm
+
+#Singular Value Decomposition
+#max components == min(nrow -1, ncol - 1)
+svd = TruncatedSVD(n_components = 850, random_state = 44)
+svd.fit(train)
+
+#The amount of variance that each PC explains
+var = svd.explained_variance_ratio_
+print(var)
+
+#Cumulative Variance explains
+var1 = np.cumsum(np.round(svd.explained_variance_ratio_, decimals=4)*100)
+plt.plot(var1)
+
+#From the plot we will select 850 principal components, which explain ... of the variance:
+print(sum(var[0:850]))
+
+#From the plot, select 
+svd = TruncatedSVD(n_components = 850, random_state = 44)
+svd.fit(train)
+train = svd.transform(train)
+test = svd.transform(test)
 
 #Convert to DMatrix
 dtrain = xgb.DMatrix(train, label = train_score)
 dtest = xgb.DMatrix(test, label = test_score)
+
+del train
+del test
+del reviews
+gc.collect()
 
 results = pd.DataFrame({'eta' : np.nan, 'max_depth' : np.nan, 'subsample' : np.nan, 'colsample_bytree' : np.nan, \
                         'gamma' : np.nan, 'min_child_weight' : np.nan, 'alpha' : np.nan, \
@@ -111,7 +142,7 @@ results = results.dropna(axis = 'rows', how = 'all')
 #Correct columns types
 results[['max_depth', 'n_rounds']] = results[['max_depth', 'n_rounds']].astype(int)
 
-#Order from best to worst score - best score - 19/02/2017 - rmse - 1.058
+#Order from best to worst score - best score - 19/02/2017 - rmse - 1.0875
 results = results.sort_values('score', ascending = False)
 
 #Train model on the full training set
@@ -145,4 +176,4 @@ test_preds_df = pd.DataFrame({
         "score": test_preds
 })
     
-test_preds_df.to_csv('model/xgb1.csv', index=False)
+test_preds_df.to_csv('model/xgb2.csv', index=False)

1095be58e08a7ceda3d3bdf626fd841cb030f6f4
Tyrone Cragg <tyrone.cragg@gmail.com>
2017-02-28 19:41:38 +0200

    xgb4

--- model/xgb.py
+++ model/xgb.py
@@ -1,29 +1,23 @@
 #Predict the product review score
-#XGB2
-
-#Using td-idf rather than dtm
 
 #Load required packages
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.preprocessing import OneHotEncoder
+from scipy.sparse import csr_matrix, hstack
 import matplotlib.pyplot as plt
 import xgboost as xgb
 from sklearn.metrics import mean_squared_error
 from sklearn.decomposition import NMF
 from sklearn.pipeline import make_pipeline
-import gc
 
 #Read in csv
 reviews = pd.read_csv('Reviews.csv', index_col = 'Id', usecols = ['Id', 'Summary', 'Text', 'Score'])
 
 #Drop duplicate score-text values
 reviews = reviews.drop_duplicates(subset = ['Summary', 'Text', 'Score'])
-
-#Remove Text
-reviews = reviews.drop('Text', axis = 'columns')
 
 #Create text - drop duplicates, convert to lower case and remove line breaks
 reviews['Summary'] = reviews['Summary'].str.lower().str.replace('<br />','')
@@ -41,48 +35,62 @@ reviews = reviews.drop('Score', axis = 'columns')
 train, test, train_score, test_score = train_test_split(reviews, score, train_size = 0.5, stratify = score)
 
 #dtm
-vectorizer = TfidfVectorizer(min_df = 0.0005, max_df = 1.0, ngram_range = (1, 3), stop_words = 'english')
+vectorizer = CountVectorizer(min_df = 0.0005, max_df = 1.0, ngram_range = (1, 3), stop_words = 'english')
 vectorizer.fit(train['Summary'])
-train_text_dtm = vectorizer.transform(train['Summary'])
-test_text_dtm = vectorizer.transform(test['Summary'])
+train_summary_dtm = vectorizer.transform(train['Summary'])
+test_summary_dtm = vectorizer.transform(test['Summary'])
 
-train = train_text_dtm
-test = test_text_dtm
+#Non-negative matrix factorisation to identify topics in Text
+#Create text - drop duplicates and convert to lower case
+train_text = train['Text'].str.lower().str.replace('<br />','').str.replace('[^\w\s]','')
+test_text = test['Text'].str.lower().str.replace('<br />','').str.replace('[^\w\s]','')
 
-"""
+#Convert to arrays
+train_text_arr = train_text.values
+test_text_arr = test_text.values
 
-#Singular Value Decomposition
-#max components == min(nrow -1, ncol - 1)
-svd = TruncatedSVD(n_components = 850, random_state = 44)
-svd.fit(train)
+#Instantiate Tfidf
+vectorizer = TfidfVectorizer(min_df = 5, ngram_range = (1, 1), stop_words = 'english')
 
-#The amount of variance that each PC explains
-var = svd.explained_variance_ratio_
-print(var)
+#Instantiate NMF
+nmf = NMF(n_components = 6, random_state = 44)
 
-#Cumulative Variance explains
-var1 = np.cumsum(np.round(svd.explained_variance_ratio_, decimals=4)*100)
-plt.plot(var1)
+#Pipeline
+pipeline = make_pipeline(vectorizer, nmf)
+train_text_arr = pipeline.fit_transform(train_text_arr)
+test_text_arr = pipeline.transform(test_text_arr)
 
-#From the plot we will select 850 principal components, which explain ... of the variance:
-print(sum(var[0:850]))
+#Determine top words for each topic
+def print_top_words(model, feature_names, n_top_words):
+    for topic_idx, topic in enumerate(model.components_):
+        print("Topic #%d:" % topic_idx)
+        print(" ".join([feature_names[i]
+                        for i in topic.argsort()[:-n_top_words - 1:-1]]))
+    print()
 
-#From the plot, select 
-svd = TruncatedSVD(n_components = 850, random_state = 44)
-svd.fit(train)
-train = svd.transform(train)
-test = svd.transform(test)
+#Call function
+print_top_words(nmf, vectorizer.get_feature_names(), 5)
 
-"""
+#Find most likely topic for each observation
+train_text_topics = train_text_arr.argmax(axis=1)
+test_text_topics = test_text_arr.argmax(axis=1)
+
+#One hot encode topics
+ohe_enc = OneHotEncoder()
+train_text_topics = ohe_enc.fit_transform(train_text_topics.reshape(-1, 1))
+test_text_topics = ohe_enc.transform(test_text_topics.reshape(-1, 1))
+
+#Convert to sparse arrays
+train_text_arr = csr_matrix(train_text_arr)
+test_text_arr = csr_matrix(test_text_arr)
+
+#Combine sparse arrays
+train = hstack([train_summary_dtm, train_text_arr, train_text_topics])
+test = hstack([test_summary_dtm, test_text_arr, test_text_topics])
 
 #Convert to DMatrix
 dtrain = xgb.DMatrix(train, label = train_score)
 dtest = xgb.DMatrix(test, label = test_score)
-
-del train
-del test
-del reviews
-gc.collect()
 
 results = pd.DataFrame({'eta' : np.nan, 'max_depth' : np.nan, 'subsample' : np.nan, 'colsample_bytree' : np.nan, \
                         'gamma' : np.nan, 'min_child_weight' : np.nan, 'alpha' : np.nan, \
@@ -148,9 +156,8 @@ results = results.dropna(axis = 'rows', how = 'all')
 #Correct columns types
 results[['max_depth', 'n_rounds']] = results[['max_depth', 'n_rounds']].astype(int)
 
-#Order from best to worst score - xgb3 best score - 19/02/2017 - rmse - 1.0549
-
-results = results.sort_values('score', ascending = False)
+#Order from best to worst score - xgb4 best score - 19/02/2017 - rmse - 1.049
+results = results.sort_values('score', ascending = True)
 
 #Train model on the full training set
 param = {'objective':'reg:linear',
@@ -183,4 +190,4 @@ test_preds_df = pd.DataFrame({
         "score": test_preds
 })
     
-test_preds_df.to_csv('model/xgb3.csv', index=False)
+test_preds_df.to_csv('model/xgb4.csv', index=False)

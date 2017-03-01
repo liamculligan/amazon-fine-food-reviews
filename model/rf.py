961a60a188037300984732d171c5df8956ffe55b
Liam Culligan <liamculligan@gmail.com>
2017-03-01 16:41:38 +0200

    Update rf

--- model/rf.py
+++ model/rf.py
@@ -2,11 +2,13 @@
 
 #Load required packages
 import pandas as pd
-import numpy as np
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+from nltk import pos_tag
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.decomposition import TruncatedSVD
-import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.preprocessing import OneHotEncoder
+from scipy.sparse import csr_matrix, hstack
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.decomposition import NMF
@@ -18,14 +20,13 @@ reviews = pd.read_csv('Reviews.csv', index_col = 'Id', usecols = ['Id', 'Summary
 #Drop duplicate score-text values
 reviews = reviews.drop_duplicates(subset = ['Summary', 'Text', 'Score'])
 
-#Remove Text
-reviews = reviews.drop('Text', axis = 'columns')
-
-#Create text - drop duplicates, convert to lower case and remove line breaks
-reviews['Summary'] = reviews['Summary'].str.lower().str.replace('<br />','')
+#Create text - remove line breaks
+reviews['Summary'] = reviews['Summary'].str.replace('<br />', ' ')
+reviews['Text'] = reviews['Text'].str.replace('<br />', ' ')
 
 #Replace nan with ""
 reviews['Summary'] = reviews['Summary'].fillna(value = "")
+reviews['Text'] = reviews['Text'].fillna(value = "")
 
 #Extract target variable
 score = reviews['Score']
@@ -33,20 +34,188 @@ score = reviews['Score']
 #Remove score from reviews
 reviews = reviews.drop('Score', axis = 'columns')
 
+#Count the number of words
+reviews['summary_count'] = reviews['Summary'].str.split().apply(len)
+reviews['text_count'] = reviews['Text'].str.split().apply(len)
+reviews = reviews.assign(all_words_count = reviews['summary_count'] + reviews['text_count'])
+
 #Create train and test stratified w.r.t score
-train, test, train_score, test_score = train_test_split(reviews, score, train_size = 0.5, stratify = score)
+train, test, train_score, test_score = train_test_split(reviews, score, train_size = 0.5, stratify = score, \
+                                                        random_state = 44)
 
-#dtm
-vectorizer = CountVectorizer(min_df = 0.0005, max_df = 1.0, ngram_range = (1, 3), stop_words = 'english')
+#Order train_score and test_score by index
+train = train.sort_index()
+train_score = train_score.sort_index()
+test = test.sort_index()
+test_score = test_score.sort_index()
+
+#Parts of Speech Tagging
+def pos_count(series):
+    
+    """Input a pandas series to convert it into a count for each part of speech."""
+    
+    rows = list(series)
+    
+    stop_words = set(stopwords.words("english"))
+    
+    #Tokenizer does not include punctuation
+    tokenizer = RegexpTokenizer(r'\w+')
+    
+    row_pos = []
+    
+    for row in rows:
+        row_tokenized = tokenizer.tokenize(row)
+        row_words = []
+        for word in row_tokenized:
+            word = word.lower()
+            if word not in stop_words:
+                #Specifically converting to lower - all words are capitalised in headline
+                row_words.append(word.lower())
+        word_pos = pos_tag(row_words)
+        pos = []
+        for tag in word_pos:
+            pos.append(tag[1])
+        row_pos.append(pos)
+        
+    #Convert list to a pandas DataFrame - each word in a separate column
+    row_pos = pd.DataFrame(row_pos)
+    row_pos.index = series.index
+    
+    #Stack to create multiindexed Series by row and column (long df)
+    #Dummy this series but still multiindexed by row and column
+    #Sum this by row to get the count of each pos for each row
+    pos_count = pd.get_dummies(row_pos.stack()).sum(level = 0)
+    
+    #Add series to to each column name
+    pos_count = pos_count.add_prefix(series.name + '_')
+    
+    return(pos_count)
+
+def pos_concat(train, test, col_name):
+    
+    """Add pos_counts to train and test ensuring that same columns exist in each."""
+    
+    train_pos = pos_count(train[col_name])
+    test_pos = pos_count(test[col_name])
+    
+    train_pos_cols = list(train_pos.columns)
+    test_pos_cols = list(test_pos.columns)
+    
+    #Remove any columns in test not in train
+    test_cols_drop = set(test_pos_cols) - set(train_pos_cols)
+    test_pos = test_pos.drop(test_cols_drop, axis = 'columns')
+        
+    #Add any columns in train not in test
+    test_cols_add = set(train_pos_cols) - set(test_pos_cols)
+    for col in test_cols_add:
+        test_pos[col] = 0
+    
+    #Add counts of pos to train and test
+    train = pd.concat([train, train_pos], axis = 'columns')
+    test = pd.concat([test, test_pos], axis = 'columns')
+    
+    #Some rows have no pos - these become nan - replace with 0
+    train[train_pos_cols] = train[train_pos_cols].fillna(value = 0)
+    test[train_pos_cols] = test[train_pos_cols].fillna(value = 0)
+     
+    return(train, test)
+
+#Call function
+train, test = pos_concat(train, test, 'Summary')
+
+#Reorder DataFrames and Series by index
+train = train.sort_index()
+train_score = train_score.sort_index()
+test = test.sort_index()
+test_score = test_score.sort_index()
+    
+#Summary dtm
+vectorizer = CountVectorizer(min_df = 0.0005, max_df = 1.0, ngram_range = (1, 3))
 vectorizer.fit(train['Summary'])
-train_text_dtm = vectorizer.transform(train['Summary'])
-test_text_dtm = vectorizer.transform(test['Summary'])
+train_summary_dtm = vectorizer.transform(train['Summary'])
+test_summary_dtm = vectorizer.transform(test['Summary'])
 
-train = train_text_dtm
-test = test_text_dtm
+#Summary dtm column names
+names_summary_dtm = vectorizer.get_feature_names()
+
+#Text dtm
+vectorizer = CountVectorizer(min_df = 0.001, max_df = 1.0, ngram_range = (1, 3))
+vectorizer.fit(train['Text'])
+train_text_dtm = vectorizer.transform(train['Text'])
+test_text_dtm = vectorizer.transform(test['Text'])
+
+#Text dtm column names
+names_text_dtm = vectorizer.get_feature_names()
+
+#Non-negative matrix factorisation to identify topics in Text
+#Remove punctuation
+train_text = train['Text'].str.replace('[^\w\s]','')
+test_text = test['Text'].str.replace('[^\w\s]','')
+
+#Convert to arrays
+train_text_arr = train_text.values
+test_text_arr = test_text.values
+
+#Instantiate Tfidf
+vectorizer = TfidfVectorizer(min_df = 5, ngram_range = (1, 1), stop_words = 'english')
+
+#Instantiate NMF
+nmf = NMF(n_components = 6, random_state = 44)
+
+#NMF pipeline
+pipeline = make_pipeline(vectorizer, nmf)
+train_text_arr = pipeline.fit_transform(train_text_arr)
+test_text_arr = pipeline.transform(test_text_arr)
+
+#NMF pipeline column names
+names_text_arr = ['nmf_0', 'nmf_1', 'nmf_2', 'nmf_3', 'nmf_4', 'nmf_5']
+
+#Determine top words for each topic
+def print_top_words(model, feature_names, n_top_words):
+    for topic_idx, topic in enumerate(model.components_):
+        print("Topic #%d:" % topic_idx)
+        print(" ".join([feature_names[i]
+                        for i in topic.argsort()[:-n_top_words - 1:-1]]))
+    print()
+
+#Call function
+print_top_words(nmf, vectorizer.get_feature_names(), 5)
+
+#Find most likely topic for each observation
+train_text_topics = train_text_arr.argmax(axis=1)
+test_text_topics = test_text_arr.argmax(axis=1)
+
+#One hot encode topics
+ohe_enc = OneHotEncoder()
+train_text_topics = ohe_enc.fit_transform(train_text_topics.reshape(-1, 1))
+test_text_topics = ohe_enc.transform(test_text_topics.reshape(-1, 1))
+
+#Add main topic column name
+names_text_topics = ['ohe_' + str(x) for x in list(ohe_enc.active_features_)]
+
+#Convert to sparse arrays
+train_text_arr = csr_matrix(train_text_arr)
+test_text_arr = csr_matrix(test_text_arr)
+
+#Remove text columns that have already been converted into numeric features
+train_features = train.drop(['Text', 'Summary'], axis = 'columns')
+test_features = test.drop(['Text', 'Summary'], axis = 'columns')
+
+#Feature names
+names_features = list(train_features.columns)
+
+#Convert train_features and test_features to sparse matrices
+train_features = csr_matrix(train_features.values)
+test_features = csr_matrix(test_features.values)
+
+#Combine sparse arrays
+train = hstack([train_summary_dtm, train_text_dtm, train_text_arr, train_text_topics, train_features])
+test = hstack([test_summary_dtm, test_text_dtm, test_text_arr, test_text_topics, test_features])
+
+col_names = names_summary_dtm + names_text_dtm + names_text_arr + names_text_topics + names_features
 
 #Create a parameter grid: map the parameter names to the values that should be searched
-param_grid = dict(max_depth = [10, 20])
+param_grid = dict(max_depth = [10])
 
 #Instantiate the grid
 rf = RandomForestRegressor(n_estimators = 100, random_state = 44, verbose = 3, n_jobs = -1)
@@ -59,7 +228,7 @@ grid.fit(train, train_score)
 #Check the scores
 scores = grid.cv_results_
 
-#Print the best score - 19/02/2017 - 1.369 (rmse - 1.170)
+#Print the best score - rf4 21/02/2017 - 1.222 (rmse - 1.105)
 print("The best score is %s" % grid.best_score_)
 print("The best model parameters are: %s" % grid.best_params_)
 
@@ -75,149 +244,4 @@ test_preds_df = pd.DataFrame({
         "score": test_preds
 })
     
-test_preds_df.to_csv('model/rf1.csv', index=False)
-
-
-"""
-
-#Define the parameter values that should be searched
-l1_ratio_range = np.arange(0, 1.2, 0.2)
-alpha_range = 10.0**-np.arange(0,4)
-
-#Create a parameter grid: map the parameter names to the values that should be searched
-param_grid = dict(l1_ratio = l1_ratio_range, alpha = alpha_range)
-print(param_grid)
-
-#Instantiate the grid
-lm = SGDRegressor(loss = 'squared_loss', n_iter = np.ceil(10**6 /train.shape[0]), random_state = 44, \
-                   verbose = 3, n_jobs = -1)
-
-grid = GridSearchCV(lm, param_grid, cv = 5, scoring = 'neg_mean_squared_error')
-
-#Fit the model
-grid.fit(train, train_score)
-
-#Check the scores
-scores = grid.cv_results_
-
-#Print the best score
-print("The best score is %s" % grid.best_score_)
-print("The best model parameters are: %s" % grid.best_params_)
-
-#Get the best model
-rf_mod = grid.best_estimator_
-
-#Fit the model on the entire training set
-rf_mod.fit(train, Popular)
-
-#Make test set predictions
-test_preds = rf_mod.predict_proba(test)[:,1]
-
-#Save the test predictions
-test_preds = pd.DataFrame({
-        "UniqueID": TestID,
-        "Probability1": test_preds
-})
-    
-test_preds.to_csv('python_practice/rf.csv', index=False)
-
-
-
-
-
-
-
-
-#Convert to array
-text_arr = text.values
-
-#Instantiate Tfidf
-vectorizer = TfidfVectorizer(min_df = 5, ngram_range = (1, 1), \
-                             stop_words = 'english')
-
-#Instantiate nmf
-nmf = NMF(n_components = 6, random_state = 44)
-
-#Pipeline
-pipeline = make_pipeline(vectorizer, nmf)
-components = pipeline.fit_transform(text_arr)
-
-#Determine top words for each topic
-def print_top_words(model, feature_names, n_top_words):
-    for topic_idx, topic in enumerate(model.components_):
-        print("Topic #%d:" % topic_idx)
-        print(" ".join([feature_names[i]
-                        for i in topic.argsort()[:-n_top_words - 1:-1]]))
-    print()
-
-#Call function
-print_top_words(nmf, vectorizer.get_feature_names(), 5)
-
-#For each topic, find the text that is the best representation of that topic
-components_scaled = (components - components.mean(axis=0))/components.std(axis=0)
-
-top_idx = np.argsort(components_scaled, axis=0)[-3:]
-
-count = 0
-for idxs in top_idx.T: 
-    print("\nTopic {}:".format(count))
-    for idx in idxs:
-        print(text.iloc[idx])
-    count += 1
-
-#Name topics
-topics_dict = {'topic' : {0 : 'generic', 1 : 'tea', 2 : 'coffee', 3: 'pets', 4 : 'shopping', 5 : 'chocolate'}}
-
-#Create a DataFrame with the nmf components for each observation
-components_df = pd.DataFrame(components, index = text.index)
-components_explore_df = pd.DataFrame(components, index = text)
-
-#DataFrame with the highest scoring topic for each observation
-topics = pd.DataFrame(components_df.idxmax(axis = 'columns').rename('topic'))
-
-#Replace topic column with their given name
-topics = topics.replace(topics_dict)
-
-#Left outer join reviews on topics
-reviews = reviews.join(topics, how = 'left')
-
-###
-
-#t-Distributed Stochastic Neighbor Embedding
-np.random.seed(44)
-indices = np.unique(np.random.randint(low = 0, high=components.shape[0], size=10000))
-
-tsne = TSNE(random_state = 44)
-tsne_embedding = tsne.fit_transform(components[indices])
-tsne_embedding = pd.DataFrame(tsne_embedding,columns=['x','y'])
-tsne_embedding['topic'] = components[indices].argmax(axis=1)
-
-#Need to create a copy - feather can't handle strided data
-tsne_feather = tsne_embedding.copy()
-feather.write_dataframe(tsne_feather, 'tsne_embedding.feather')
-
-scatter = plt.scatter(data=tsne_embedding,x='x',y='y',s=6,c=tsne_embedding['topic'],cmap="Set1")
-plt.axis('off')
-plt.savefig('tsne.png', dpi = 500)
-plt.show()
-
-
-###
-
-#Quick recommender system
-from sklearn.preprocessing import normalize 
-components_normalised = normalize(components)
-
-#Create df
-df = pd.DataFrame(components_normalised, index = text)
-
-# Select the row corresponding to ... review
-article = df.iloc[0]
-
-# Compute the dot products: similarities
-similarities = df.dot(article)
-
-# Display those with the largest cosine similarity
-print(similarities.nlargest())
-
-"""
+test_preds_df.to_csv('model/rf4.csv', index=False)
